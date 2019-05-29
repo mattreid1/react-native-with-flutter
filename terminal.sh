3ff9cb2f1e436005398b88e498ef3f56fba3667f
Matt Reid <matthewreid2000@gmail.com>
2019-05-29 17:44:10 +0100

    Initial commit. Created Flutter project and RN basics.

---
+++ terminal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Description: The commands ran so far
+# Author: Matt Reid (mattreid1) - 29/05/2019
+
+mkdir RN
+mkdir Flutter
+cd RN
+yarn add react-native
+yarn add react@16.8.3
+cd ../Flutter
+flutter create rnflutter
